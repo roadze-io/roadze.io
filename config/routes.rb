80fe8a37a16459a62ed5dc6134e5b7fb0f35b618
TheMindlessDev <themindlessdev@protonmail.com>
2020-05-02 14:35:24 -0600

    Cleans up routes file, adds route constraints and updates project dictionary

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 # frozen_string_literal: true
 
+# roadze.io
+class Marketing
+  def self.matches?(request)
+    request.subdomain.blank? || request.subdomain == 'www'
+  end
+end
+
+# app.roadze.io
+class Subscription
+  def self.matches?(request)
+    request.subdomain.present? && request.subdomain == 'app'
+  end
+end
+
+# support.roadze.com
+class Support
+  def self.matches?(request)
+    request.subdomain.present? && request.subdomain == 'support'
+  end
+end
+
+# Application Routes
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints(Marketing) do
+  end
+
+  constraints(Subscription) do
+  end
+
+  constraints(Support) do
+  end
 end
