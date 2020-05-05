7ed854f23a13421b2dacea30bc7b262caed8aba8
TheMindlessDev <themindlessdev@protonmail.com>
2020-05-04 23:02:29 -0600

    Back out timezone around action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,9 @@
 
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-  around_action :set_time_zone
   before_action :validate_cookie_presnece
 
   private
-
-  def set_time_zone(&block)
-    if admin_signed_in?
-      Time.use_zone(current_admin.time_zone, &block)
-    else
-
-    end
-  end
 
   def validate_cookie_presnece
     return unless cookies.present?
